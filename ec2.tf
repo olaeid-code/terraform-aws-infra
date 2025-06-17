442f5e167dfd981e5d17876ca24eba79e13a90b1
ola <olaeid460@gmail.com>
2025-06-17 13:07:27 +0300

    Deleted module and cleaned structure

--- ec2.tf
+++ ec2.tf
@@ -2,7 +2,7 @@
 resource "aws_instance" "bastion" {
   ami                    = "ami-0c02fb55956c7d316" # Amazon Linux 2 (اختيار حسب المنطقة)
   instance_type          = var.instance_type       #"t2.micro"
-  subnet_id              = aws_subnet.public_subnet_1.id
+  subnet_id              = aws_subnet.public1_subnet.id
   vpc_security_group_ids = [aws_security_group.bastion_sg.id]
   key_name               = aws_key_pair.bastion_key.key_name
 
@@ -15,7 +15,7 @@ resource "aws_instance" "bastion" {
 resource "aws_instance" "app" {
   ami                    = var.ami           #"ami-0c02fb55956c7d316"
   instance_type          = var.instance_type #"t2.micro"
-  subnet_id              = aws_subnet.private_subnet_1.id
+  subnet_id              = aws_subnet.private1_subnet.id
   vpc_security_group_ids = [aws_security_group.app_sg.id]
   key_name               = aws_key_pair.bastion_key.key_name
 
